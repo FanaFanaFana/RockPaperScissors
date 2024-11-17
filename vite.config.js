373b1fbd8e8eb2b796e5f0cd4ea24f238906ed1c
FanaFanaFana <y.croes@freenet.de>
2024-11-17 16:42:46 +0100

    wef

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: './', // Ensures relative paths for assets
+  build: {
+    outDir: 'dist', // Default output directory
+  
+  },
+});
+
